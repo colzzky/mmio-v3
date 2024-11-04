8c61a2f6aaad5239551552cf6bc02b9062108c25
clakr <clarktolosa@gmail.com>
2024-11-04 09:58:02 +0800

    refactor(settings): refactored settings-related routes and layout

--- src/modules/settings/views/AccountPage.vue
+++ src/modules/settings/views/AccountPage.vue
@@ -6,39 +6,36 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/core/components/ui/avatar
 import { Button } from '@/core/components/ui/button'
 import { Label } from '@/core/components/ui/label'
 import { Switch } from '@/core/components/ui/switch'
-import SettingsLayout from '@/core/layouts/SettingsLayout.vue'
 </script>
 
 <template>
-  <SettingsLayout>
-    <main class="grid grid-cols-[minmax(350px,1fr)_1fr] gap-x-4 xl:gap-x-12 2xl:gap-x-16">
-      <section class="flex flex-col gap-y-8 divide-y">
-        <div class="flex flex-col gap-y-3">
-          <UpdateAccountName />
-          <UpdateEmail />
-          <ChangePassword />
+  <main class="grid grid-cols-[minmax(350px,1fr)_1fr] gap-x-4 xl:gap-x-12 2xl:gap-x-16">
+    <section class="flex flex-col gap-y-8 divide-y">
+      <div class="flex flex-col gap-y-3">
+        <UpdateAccountName />
+        <UpdateEmail />
+        <ChangePassword />
+      </div>
+      <div class="flex flex-col gap-y-4 py-8">
+        <h2 class="text-lg font-bold">Newsletter</h2>
+        <div class="flex items-center justify-between">
+          <Label for="newsletter" class="text-xs"
+            >I want to get a newsletter from the company</Label
+          >
+          <Switch id="newsletter" />
         </div>
-        <div class="flex flex-col gap-y-4 py-8">
-          <h2 class="text-lg font-bold">Newsletter</h2>
-          <div class="flex items-center justify-between">
-            <Label for="newsletter" class="text-xs"
-              >I want to get a newsletter from the company</Label
-            >
-            <Switch id="newsletter" />
-          </div>
-        </div>
-      </section>
-      <section class="flex flex-col items-center gap-y-4">
-        <h2 class="text-lg font-bold">Your Photo</h2>
-        <Avatar class="size-24">
-          <AvatarImage src="https://placehold.co/96" />
-          <AvatarFallback>UI</AvatarFallback>
-        </Avatar>
-        <div class="flex items-center justify-center gap-x-2">
-          <Button size="sm">Update</Button>
-          <Button size="sm" variant="destructive">Remove</Button>
-        </div>
-      </section>
-    </main>
-  </SettingsLayout>
+      </div>
+    </section>
+    <section class="flex flex-col items-center gap-y-4">
+      <h2 class="text-lg font-bold">Your Photo</h2>
+      <Avatar class="size-24">
+        <AvatarImage src="https://placehold.co/96" />
+        <AvatarFallback>UI</AvatarFallback>
+      </Avatar>
+      <div class="flex items-center justify-center gap-x-2">
+        <Button size="sm">Update</Button>
+        <Button size="sm" variant="destructive">Remove</Button>
+      </div>
+    </section>
+  </main>
 </template>
--- src/modules/settings/views/ApiIntegrationPage.vue
+++ src/modules/settings/views/ApiIntegrationPage.vue
@@ -1,36 +1,23 @@
 <script setup lang="ts">
-import SettingsLayout from '@/core/layouts/SettingsLayout.vue';
-import { onBeforeMount, onMounted, ref, watch } from 'vue';
-import { useAuthStore } from '@/stores/authStore';
-import { usePlatformAPIStore } from '@/stores/platformAPIStore';
-import { uiHelpers } from '@/core/utils/ui-helper';
-import type { Timestamp } from 'firebase/firestore';
-import MetaApiIntegration from '../components/APIIntegrationSettings/MetaApiIntegration.vue';
-import { useRoute, useRouter } from 'vue-router';
-const router = useRouter()
-const route = useRoute()
-const useAuth = useAuthStore()
-const platformApiStore = usePlatformAPIStore()
-const { platformAPI, platform_api_list } = platformApiStore
-const { user_auth } = useAuth
+import MetaApiIntegration from '../components/APIIntegrationSettings/MetaApiIntegration.vue'
+import { usePlatformAPIStore } from '@/stores/platformAPIStore'
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 
-//Use this if you want a more dynamic loading
-const pageLoad = ref<boolean>(false)
-const pageDataLoad = ref<boolean>(true)
+const route = useRoute()
+const platformApiStore = usePlatformAPIStore()
+const { platform_api_list } = platformApiStore
 
 onMounted(async () => {
-    await platform_api_list.initializeAccountApis()
+  await platform_api_list.initializeAccountApis()
 })
-
-
 </script>
 <template>
-    <SettingsLayout>
-        <main>
-            <MetaApiIntegration
-                v-if="route.name === 'settings-api-integrations-meta' || route.name === 'settings-api-integrations'" />
-        </main>
-    </SettingsLayout>
+  <main>
+    <MetaApiIntegration
+      v-if="route.name === 'api-integrations-meta' || route.name === 'api-integrations'"
+    />
+  </main>
 </template>
 
 <style></style>
